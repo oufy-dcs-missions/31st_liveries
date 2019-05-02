04d7394b21720bb9da0720608423cf9d19e45784
Sebastien LONGO (IT-CE) <sebastien.longo@it-ce.fr>
2019-05-02 09:57:05 +0200

    initial commit

---
+++ M-2000C/ETD 2'7 Argonne 3eme EC 3-XD/description.lua
@@ -0,0 +1,76 @@
+livery = {
+	{"ext1", 0 ,"M2KC_1T_3-XD",false};
+	{"ext1", 13 ,"M2KC_1T_RoughMet_3-XD",false};
+	{"ext2", 0 ,"M2KC_2T_3-XD",false};	
+	{"ext6", 0 ,"M2KC_6T_3-XD",false};
+	{"ext6", 2 ,"M2KC_6Specular_3-XD",false};
+	{"ext6", 13 ,"M2KC_6T_RoughMetory3-XD",false};
+	{"pilot_M2KC_helmet", 0 ,"M2KC_8T_3-XD",false};
+	
+	{"FUSE_NUMBER1_001", 0 ,"M2KC_1T_3-XD",false};
+	{"FUSE_NUMBER1_001", DECAL ,"empty",false};
+
+	{"FUSE_NUMBER2_001", 0 ,"M2KC_1T_3-XD",false};
+	{"FUSE_NUMBER2_001", DECAL ,"empty",false};
+
+	{"FUSE_NUMBER2_010", 0 ,"M2KC_1T_3-XD",false};
+	{"FUSE_NUMBER2_010", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER1_001", 0 ,"M2KC_2T_3-XD",false};
+	{"TAIL_NUMBER1_001", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER1_010", 0 ,"M2KC_2T_3-XD",false};
+	{"TAIL_NUMBER1_010", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER1_100", 0 ,"M2KC_2T_3-XD",false};
+	{"TAIL_NUMBER1_100", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER2_001", 0 ,"M2KC_2T_3-XD",false};
+	{"TAIL_NUMBER2_001", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER2_010", 0 ,"M2KC_2T_3-XD",false};
+	{"TAIL_NUMBER2_010", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER2_100", 0 ,"M2KC_2T_3-XD",false};
+	{"TAIL_NUMBER2_100", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER3_001", 0 ,"M2KC_2T_3-XD",false};
+	{"TAIL_NUMBER3_001", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER3_010", 0 ,"M2KC_2T_3-XD",false};
+	{"TAIL_NUMBER3_010", DECAL ,"empty",false};
+
+	{"TAIL_NUMBER3_100", 0 ,"M2KC_2T_3-XD",false};
+	{"TAIL_NUMBER3_100", DECAL ,"empty",false};
+
+	{"NOSE_NUMBER_001", 0 ,"M2KC_1T_3-XD",false};
+	{"NOSE_NUMBER_001", DECAL ,"empty",false};
+
+	{"NOSE_NUMBER_010", 0 ,"M2KC_1T_3-XD",false};
+	{"NOSE_NUMBER_010", DECAL ,"empty",false};
+
+	{"NOSE_NUMBER_100", 0 ,"M2KC_1T_3-XD",false};
+	{"NOSE_NUMBER_100", DECAL ,"empty",false};
+
+	{"WINGR_NUMBER_001", 0 ,"M2KC_2T_3-XD",false};
+	{"WINGR_NUMBER_001", DECAL ,"empty",false};
+
+	{"WINGR_NUMBER_010", 0 ,"M2KC_2T_3-XD",false};
+	{"WINGR_NUMBER_010", DECAL ,"empty",false};
+
+	{"WINGR_NUMBER_100", 0 ,"M2KC_2T_3-XD",false};
+	{"WINGR_NUMBER_100", DECAL ,"empty",false};
+
+	{"WINGL_NUMBER_001", 0 ,"M2KC_1T_3-XD",false};
+	{"WINGL_NUMBER_001", DECAL ,"empty",false};
+
+	{"WINGL_NUMBER_010", 0 ,"M2KC_1T_3-XD",false};
+	{"WINGL_NUMBER_010", DECAL ,"empty",false};
+
+	{"WINGL_NUMBER_100", 0 ,"M2KC_1T_3-XD",false};
+	{"WINGL_NUMBER_100", DECAL ,"empty",false};
+}
+name = "ETD 2/7 Argonne 3eme EC 3-XD"
+countries = {"FRA"}
+
+
